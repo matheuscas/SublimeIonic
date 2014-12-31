887c6e3549249d5c63c312767a041f82dc867ab5
matheuscas <matheus.mcas@gmail.com>
2014-12-30 23:02:05 -0300

    completions file was renamed

--- html_completions.py
+++ html_completions.py
@@ -1,11 +1,15 @@
-import sublime, sublime_plugin
+import sublime
+import sublime_plugin
 
 # Provide completions that match just after typing an opening angle bracket
+
+
 class ExtensionsCompletions(sublime_plugin.EventListener):
+
     def on_query_completions(self, view, prefix, locations):
         # Only trigger within HTML
         if not view.match_selector(locations[0],
-                "text.html - source"):
+                                   "text.html - source"):
             return []
 
         pt = locations[0] - len(prefix) - 1
@@ -14,28 +18,38 @@ class ExtensionsCompletions(sublime_plugin.EventListener):
             return []
 
         headers_footers = [
-            ("ion-header-bar\tIonic Extension", 'ion-header-bar align-title="${1:left}" class="${2:bar-positive}">$0</ion-header-bar>'),
-            ("ion-footer-bar\tIonic Extension", 'ion-footer-bar align-title="${1:left}" class="${2:bar-assertive}">$0</ion-footer-bar>')
+            ("ion-header-bar\tIonic Extension",
+             'ion-header-bar align-title="${1:left}" class="${2:bar-positive}">$0</ion-header-bar>'),
+            ("ion-footer-bar\tIonic Extension",
+             'ion-footer-bar align-title="${1:left}" class="${2:bar-assertive}">$0</ion-footer-bar>')
         ]
 
         content = [
-            ("ion-content\tIonic Extension", 'ion-content has-subheader="${1:false}">$0</ion-content>'),
-            ("ion-refresher\tIonic Extension", 'ion-refresher pulling-text="${1:Pull to refresh...}" on-refresh="${2:doRefresh()}">$0</ion-refresher>'),
+            ("ion-content\tIonic Extension",
+             'ion-content has-subheader="${1:false}">$0</ion-content>'),
+            ("ion-refresher\tIonic Extension",
+             'ion-refresher pulling-text="${1:Pull to refresh...}" on-refresh="${2:doRefresh()}">$0</ion-refresher>'),
             ("ion-pane\tIonic Extension", 'ion-pane>$0</ion-pane>')
         ]
 
         lists = [
             ("ion-list\tIonic Extension", 'ion-list>$0</ion-list>'),
             ("ion-item\tIonic Extension", 'ion-item href="#">$0</ion-item>'),
-            ("ion-delete-button\tIonic Extension", 'ion-delete-button class="${1:ion-minus-circled}">$0</ion-delete-button>'),
-            ("ion-reorder-button\tIonic Extension", 'ion-reorder-button class="${1:ion-navicon}" on-reorder="${2:moveItem(item, \$fromIndex, \$toIndex)}">$0</ion-reorder-button>'),
-            ("ion-option-button\tIonic Extension", 'ion-option-button class="${1:button-positive}">$0</ion-option-button>')
+            ("ion-delete-button\tIonic Extension",
+             'ion-delete-button class="${1:ion-minus-circled}">$0</ion-delete-button>'),
+            ("ion-reorder-button\tIonic Extension",
+             'ion-reorder-button class="${1:ion-navicon}" on-reorder="${2:moveItem(item, \$fromIndex, \$toIndex)}">$0</ion-reorder-button>'),
+            ("ion-option-button\tIonic Extension",
+             'ion-option-button class="${1:button-positive}">$0</ion-option-button>')
         ]
 
         forms = [
-            ("ion-checkbox\tIonic Extension", 'ion-checkbox ng-model="${1:isChecked}">${2:Checkbox Label}</ion-checkbox>'),
-            ("ion-radio\tIonic Extension",'ion-radio ng-model="${1:choice}" ng-value="${2:\'A\'}">${3:Choose A}</ion-radio>'),
-            ("ion-toogle\tIonic Extension", 'ion-toggle ng-model="${1:airplaneMode}" toggle-class="${2:toggle-calm}">${3:Airplane Mode}</ion-toggle>')
+            ("ion-checkbox\tIonic Extension",
+             'ion-checkbox ng-model="${1:isChecked}">${2:Checkbox Label}</ion-checkbox>'),
+            ("ion-radio\tIonic Extension",
+             'ion-radio ng-model="${1:choice}" ng-value="${2:\'A\'}">${3:Choose A}</ion-radio>'),
+            ("ion-toogle\tIonic Extension",
+             'ion-toggle ng-model="${1:airplaneMode}" toggle-class="${2:toggle-calm}">${3:Airplane Mode}</ion-toggle>')
         ]
 
         # tabs = [
@@ -47,7 +61,7 @@ class ExtensionsCompletions(sublime_plugin.EventListener):
         #     ("ion-side-menus\tIonic Extension", 'ion-side-menus>$0</ion-side-menus>'),
         #     ("ion-side-menu-content\tIonic Extension", 'ion-side-menu-content drag-content="${1:true}">$0</ion-side-menu-content>'),
         #     ("ion-side-menu\tIonic Extension", 'ion-side-menu side="${1:left}">$0</ion-side-menu>')
-        # ] 
+        # ]
 
         # navigation = [
         #     ("ion-nav-view\tIonic Extension", 'ion-nav-view animation="${1:slide-left-right}">$0</ion-nav-view>'),
@@ -92,7 +106,7 @@ class ExtensionsCompletions(sublime_plugin.EventListener):
 
         # completions =  tabs + side_menus + navigation + headers_footers + \
         #                 content + scroll + lists + form_inputs + \
-        #                 slide_box + events   
+        #                 slide_box + events
 
         completions = headers_footers + content + lists + forms
 
